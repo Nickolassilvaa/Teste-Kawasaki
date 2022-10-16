7cd41412181aea950c96bd2a6d40efac69cba0c3
nrp-silva <nickolascaragua@gmail.com>
2022-10-16 17:53:24 -0300

    first commit

---
+++ lista_produtos/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using lista_produto;
+using Microsoft.EntityFrameworkCore;
+
+namespace lista_produtos.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ lista_produtos/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace lista_produto
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string ProdutoId { get; set; } = string.Empty;
+        public string ProdutoDescricao { get; set; } = string.Empty;
+        public decimal ProdutoPreco { get; set; }
+    }
+}
